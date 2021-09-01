44225d5a51edae7196df6ec7c577c768f8d9cd26
billiejoenahema <billiejoenahema@gmail.com>
2021-09-01 20:42:56 +0900

    improve: change button size

--- src/components/VolumeButton.js
+++ src/components/VolumeButton.js
@@ -1,17 +1,26 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
 import { IconButton } from '@material-ui/core'
 import VolumeOffIcon from '@material-ui/icons/VolumeOff'
 import VolumeUpIcon from '@material-ui/icons/VolumeUp'
 
+const useStyles = makeStyles({
+  icon: {
+    height: 36,
+    width: 36,
+  }
+})
+
 const VolumeButton = ({ isLocal, muted, setMuted, rtcClient }) => {
   const Icon = muted ? VolumeOffIcon : VolumeUpIcon
+  const classes = useStyles()
 
   return (
     <IconButton onClick={() => {
       setMuted((prev) => !prev)
       if (isLocal) rtcClient.toggleAudio()
     }} aria-label="switch mute">
-      <Icon />
+      <Icon className={classes.icon} />
     </IconButton>
   )
 }
